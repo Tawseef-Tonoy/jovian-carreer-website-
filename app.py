1dba324beebcd7efd5db990d8d3219c2f5d0e5d5
Tawseef-Tonoy <tawseef.ul.islam555@gmail.com>
2025-03-27 22:06:27 +0600

    moving to ubuntu

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def hello_world():
-    return "hello world"
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
